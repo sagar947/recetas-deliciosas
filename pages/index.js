44327523a5241e9f5a830cbea05e220c624568e5
Yashwanth Sagar <thelostwatermelon@gmail.com>
2020-11-13 10:26:36 +0530

    removed .next folder from vcs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import Link from 'next/link'
+import Head from 'next/head';
 import RecipesList from '../components/recipes-list';
 import { Container, Row } from 'react-bootstrap';
 
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>Home - Deliciosas Recitas</title>
+      </Head>
       <Container>
         <RecipesList />
       </Container>
